e5a90d140449e01486cc5369570021a037cdda65
风筝 <1954814839@qq.com>
2024-11-23 19:20:58 +0800

    创建FP模板项目

---
+++ README.md
@@ -0,0 +1,2 @@
+# FPSDemo
+基于UE5.4的游戏First Person模板开发
---
+++ Source/ShootDemo/ShootDemo.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShootDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShootDemo, "ShootDemo" );
+ 
